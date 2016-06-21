77f9ed3db721a375e22b72472ade0c0744265b71
Joakim Hove <joakim.hove@gmail.com>
2016-06-22 00:21:55 +0200

    Added stack to friskbyClient

--- lib/friskby_client.py
+++ lib/friskby_client.py
@@ -11,14 +11,26 @@ class FriskbyClient(object):
         self.server_url = server_url
         self.sensor_id = sensor_id
         self.post_key = post_key
+        self.stack = []
 
 
     def post(self , value):
         timestamp = datetime.datetime.utcnow().isoformat()
+        self.stack.insert( (timestamp, value) )
 
-        data = {"value"     : value,
-                "sensorid"  : self.sensor_id,
-                "timestamp" : timestamp,
-                "key"       : self.post_key }
+        while True:
+            pair = self.stack.pop( )
 
-        requests.post( self.server_url , headers=FriskbyClient.headers, data=json.dumps(data))
+            data = {"timestamp" : pair[0],
+                    "sensorid"  : self.sensor_id,
+                    "value"     : pair[1],
+                    "key"       : self.post_key }
+
+            respons = requests.post( self.server_url , headers=FriskbyClient.headers, data=json.dumps(data))
+            if response.status_code != 201:
+                stack.append( pair )
+                break
+                
+            if len(self.stack) == 0:
+                break
+
