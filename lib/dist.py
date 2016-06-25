a11426149fa3def5506d9447b3b0814d000913b8
Joakim Hove <joakim.hove@gmail.com>
2016-06-25 10:24:34 +0200

    Fixed typo

--- lib/dist.py
+++ lib/dist.py
@@ -2,7 +2,7 @@ files = ["bin/fby_client",
          "lib/git_module.py",
          "lib/device_config.py",
          "lib/dist.py",
-         "lib/sampler",
+         "lib/sampler.py",
          "lib/sds011.py",
          "lib/friskby_client.py"]
 
