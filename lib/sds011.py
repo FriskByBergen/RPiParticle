d537f37effaffde72ef726efe9c23556a21aeb4a
Joakim Hove <joakim.hove@gmail.com>
2016-06-25 11:58:44 +0200

    Using ord in read

--- lib/sds011.py
+++ lib/sds011.py
@@ -18,15 +18,15 @@ class SDS011(object):
             if ord(s) == int("C0",16):
                 s = self.device.read(7)
                 
-                pm25hb = s[0]
-                pm25lb = s[1]
-                pm10hb = s[2]
-                pm10lb = s[3]
+                pm25hb = ord(s[0])
+                pm25lb = ord(s[1])
+                pm10hb = ord(s[2])
+                pm10lb = ord(s[3])
 
-                cs     = s[6]
+                cs     = ord(s[6])
                 # we should verify the checksum... it is the sum of bytes 1-6 truncated...
 
-                pm25 = float(pm2hb + pm2lb*256)/10.0
+                pm25 = float(pm25hb + pm25lb*256)/10.0
                 pm10 = float(pm10hb + pm10lb*256)/10.0
                 
                 return (pm10 , pm25)
