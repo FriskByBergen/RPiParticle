1a4277cbacebc3cf4a6fa62de23377fa8d736ae0
Joakim Hove <joakim.hove@gmail.com>
2016-06-25 11:26:31 +0200

    More sample

--- tests/test_sds011.py
+++ tests/test_sds011.py
@@ -17,12 +17,12 @@ class SDS011Test(TestCase):
     
     @skipUnless( have_SDS011, "Must be on RPi with /dev/ttyUSB0 to run test")
     def test_read(self):
-        pm10, pm25 = SDS011.read()
+        pm10, pm25 = SDS011(True).read()
         
     
     @skipIf( have_SDS011 , "Test based on Mock SDS011")
     def test_read_mock(self):
-        pm10 , pm25 = SDS011.read()
+        pm10 , pm25 = SDS011(True).read()
         self.assertEqual( pm10 , 10 )
         self.assertEqual( pm25 , 25 )
 
