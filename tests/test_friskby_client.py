a3916284efd84aa6e73d814c7240c2380fb34863
Joakim Hove <joakim.hove@gmail.com>
2016-08-18 20:22:31 +0200

    Added import test for fby_client

--- tests/test_friskby_client.py
+++ tests/test_friskby_client.py
@@ -1,8 +1,10 @@
 import tempfile 
 from unittest import TestCase, skipUnless
+import os
 import os.path
 import stat
 import requests
+import imp
 from friskby_client import FriskbyClient
 from device_config import DeviceConfig
 
@@ -19,3 +21,12 @@ class FriskbyClientCTest(TestCase):
     def test_client(self):
         device_config = DeviceConfig.download( "https://friskby.herokuapp.com/sensor/api/device/FriskPI03/")
         client = FriskbyClient(device_config , "SensorID")
+
+    
+    def test_import(self):
+        client_path = os.path.join( os.path.dirname( __file__ ) , "../bin/fby_client")
+        self.assertTrue( os.path.isfile( client_path ) )
+
+        os.environ["FRISKBY_TEST"] = "True"
+        client = imp.load_source( "client" , client_path )
+        del os.environ["FRISKBY_TEST"]
