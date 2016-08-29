a67aab606538e1248b7f583648319e379526dbfa
Joakim Hove <joakim.hove@gmail.com>
2016-08-29 23:34:30 +0200

    xxxx

--- tests/test_sampler.py
+++ tests/test_sampler.py
@@ -25,5 +25,6 @@ class SamplerTest(TestCase):
         stop = datetime.datetime.now( )
         
         dt = stop - start
+        print "Elapsed:%g  %g" % (dt.total_seconds() , 2*sleep_time)
         self.assertTrue( (dt.total_seconds() - sample_time) <= 2*sleep_time )
         
