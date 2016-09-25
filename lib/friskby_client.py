31f7c03c349fd7f4582f494be922e5ebe11adf29
Joakim Hove <joakim.hove@gmail.com>
2016-09-25 10:42:04 +0200

    Using new value_list parameter when posting

--- lib/friskby_client.py
+++ lib/friskby_client.py
@@ -21,31 +21,56 @@ class FriskbyClient(object):
                     pass
             os.unlink( self.cache_file )
 
+
+    def post_value(self , value):
+        timestamp = datetime.datetime.utcnow().isoformat()
+        try:
+            data = {"timestamp" : timestamp,
+                    "sensorid"  : self.sensor_id,
+                    "value"     : value,
+                    "key"       : self.device_config.getPostKey( ) }
+
+            respons = requests.post( self.device_config.getPostURL( ) , 
+                                     headers=FriskbyClient.headers, 
+                                     data=json.dumps(data))
+
+            if respons.status_code != 201:
+                self.stack.append( (timestamp , value) )
+                break
+        except:
+            self.stack.append( (timestamp , value) )
+
+
+    def post_stack(self):
+        data = {"timestamp"  : timestamp,
+                "sensorid"   : self.sensor_id,
+                "value_list" : self.stack,
+                "key"        : self.device_config.getPostKey( ) }
+
+        try:
+            respons = requests.post( self.device_config.getPostURL( ) , 
+                                     headers=FriskbyClient.headers, 
+                                     data=json.dumps(data))
+            if respons.status_code != 201:
+                raise Exception("Recieved wrong status code")
+            self.stack = []
+        except:
+            pass
+
+        if len(self.stack) == 0:
+            if os.path.isfile( self.cache_file ):
+                os.unlink( self.cache_file )
+        else:
+            with open(self.cache_file , "w") as f:
+                f.write( json.dumps( self.stack ))
+                    
+
             
 
     def post(self , value):
-        timestamp = datetime.datetime.utcnow().isoformat()
-        self.stack.append( (timestamp, value) )
+        if len(self.stack) == 0:
+            self.post_value( value )
 
-        while True:
-            pair = self.stack.pop( )
-
-            data = {"timestamp" : pair[0],
-                    "sensorid"  : self.sensor_id,
-                    "value"     : pair[1],
-                    "key"       : self.device_config.getPostKey( ) }
-
-            try:
-                respons = requests.post( self.device_config.getPostURL( ) , headers=FriskbyClient.headers, data=json.dumps(data))
-                if respons.status_code != 201:
-                    self.stack.append( pair )
-                    break
-            except:
-                self.stack.append( pair )
-                with open(self.cache_file , "w") as f:
-                    f.write( json.dumps( self.stack ))
-                break
-                
-            if len(self.stack) == 0:
-                break
-
+        if len(self.stack) > 0:
+            self.post_stack( )
+        
