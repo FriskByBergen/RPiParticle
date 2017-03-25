e053ae3adf1143c8bd892c524567fbcdb3faa729
Pål Grønås Drange <PGDR@statoil.com>
2017-03-25 10:38:36 +0100

    fixed lint tests

--- tests/test_pylint.py
+++ tests/test_pylint.py
@@ -1,48 +1,44 @@
-import os
-import requests
-import shutil
+from os import listdir
+from os.path import isfile, join
 import sys
-import json
-import tempfile
 from unittest import TestCase, skipUnless
-import os.path
-import stat
 import subprocess
-
-from friskby import DeviceConfig
-from context import TestContext
-
 
 try:
     subprocess.call(["pylint", "--version"])
-    have_pylint = True
+    HAVE_PYLINT = True
 except OSError:
-    sys.stderr.write('** Warning: could not find Python checker "pylint" - static checks skipped')
-    have_pylint = False
-
+    msg = '** Warning: Could not find pylint. Static checks skipped'
+    sys.stderr.write(msg)
+    HAVE_PYLINT = False
 
 class PylintTest(TestCase):
-    ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 
-    @skipUnless(have_pylint,"Must have pylint executable installed")
-    def test_binaries(self):
-        sys.path.insert(0, os.path.join(self.ROOT, "lib"))
-        for prog in ["bin/fby_client", "bin/initrpi"]:
-            exit_code = subprocess.check_call(["pylint", "-E", prog])
-            self.assertEqual(exit_code, 0)
+    def _is_python_file(self, path, fname):
+        fpath = join(path, fname)
+        if not isfile(fpath):
+            return False
+        if fname[-3:] == '.py':
+            return True
+        if fname[:4] == 'fby_':
+            return True
+        if fname == 'run_tests':
+            return True
+        return False
 
+    def _do_test_files(self, path):
+        """pylint -E on all .py files in path"""
+        for fname in listdir(path):
+            if len(fname) > 2 and self._is_python_file(path, fname):
+                fpath = join(path, fname)
+                retcode = subprocess.call(["pylint", "-E", fpath])
+                self.assertEqual(0, retcode,
+                                 msg='linting required for %s' % fpath)
 
-    @skipUnless(have_pylint,"Must have pylint executable installed")
+    @skipUnless(HAVE_PYLINT, "Must have pylint executable installed")
     def test_library(self):
-        module_path = 'python/friskby/'
-        for lib in [module_path + 'ts.py',
-                    module_path + 'device_config.py',
-                    module_path + 'dist.py',
-                    module_path + 'git_module.py',
-                    module_path + 'sds011.py',
-                    module_path + 'wifi_config.py',
-                    module_path + 'friskby_dao.py',
-                    module_path + 'os_release.py']:
+        self._do_test_files('bin/')
 
-            exit_code = subprocess.check_call(["pylint", "-E", lib])
-            self.assertEqual(exit_code, 0)
+    @skipUnless(HAVE_PYLINT, "Must have pylint executable installed")
+    def test_tests_meta(self):
+        self._do_test_files('tests/')
