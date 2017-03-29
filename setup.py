73d761ab4dd450b73d9210c463f48187583d35d3
Jonas G. Drange <jonas@drange.net>
2017-03-29 12:00:55 +0200

    adds local settings semantics, and file

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='RPiParticle',
-    version='0.7.0',
+    version='0.7.1',
     description='Raspberry Pi and SDS011 particle sensor posting data ',
     long_description=long_description,
     url='https://github.com/FriskByBergen/',
@@ -33,7 +33,14 @@ setup(
         'friskby',
         'friskby-controlpanel',
     ],
+    packages=['rpiparticle'],
     data_files=[
+        (
+            './etc/friskby/',
+            [
+                'data/friskby-settings.json'
+            ]
+        ),
         (
             './bin/',
             [
