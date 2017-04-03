be5f57576249871e3f4db0ef792531f9379464ba
Pål Grønås Drange <PGDR@statoil.com>
2017-04-03 23:22:34 +0200

    deleted unused test, added more test, update to demeter

--- tests/test_friskby_integration.py
+++ tests/test_friskby_integration.py
@@ -1,7 +1,13 @@
 from unittest import TestCase
 from random import randint
 
-from friskby import TS
+from friskby import TS, FriskbyDao, FriskbySampler
+
+class MockSDS011(object):
+    def __init__(self, device_url):
+        self.device_url = device_url
+    def read(self):
+        return (10.0, 2.5)
 
 class FriskbyIntegration(TestCase):
 
@@ -10,3 +16,12 @@ class FriskbyIntegration(TestCase):
         t.append(4)
         t.append(6)
         self.assertEqual(5.0, t.mean())
+
+    def test_dao_sampler(self):
+        dao_sql_fname = '/tmp/rpiparticle/%d__.sql' % randint(2**42, 2**52)
+        dao = FriskbyDao(dao_sql_fname)
+        sampler = FriskbySampler(MockSDS011('/path/to/dev'), dao,
+                                 sample_time=0.1, sleep_time=0.1)
+        sampler.collect()
+        data = dao.get_non_uploaded(limit=30)
+        self.assertTrue(len(data) > 0)
