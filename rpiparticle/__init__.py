19374d2bce23dc129142b59d5bf1d5ba735bd86a
Pål Grønås Drange <PGDR@statoil.com>
2017-04-03 23:23:49 +0200

    bump version 0.8.1

--- rpiparticle/__init__.py
+++ rpiparticle/__init__.py
@@ -7,7 +7,7 @@ webserver for updates, and if a new version has been configured the script will
 invoke git to download a new version and restart itself.
 
 """
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 
 from .fby_settings import get_setting, get_settings
 
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='RPiParticle',
-    version='0.8.0',
+    version='0.8.1',
     description='Raspberry Pi and SDS011 particle sensor posting data ',
     long_description=long_description,
     url='https://github.com/FriskByBergen/',
