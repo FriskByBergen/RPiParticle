ec0ad39bbb024601cc66f398df2119d06cffe149
Pål Grønås Drange <PGDR@statoil.com>
2017-04-04 21:55:09 +0200

    bump version 0.8.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='RPiParticle',
-    version='0.8.1',
+    version='0.8.2',
     description='Raspberry Pi and SDS011 particle sensor posting data ',
     long_description=long_description,
     url='https://github.com/FriskByBergen/',
