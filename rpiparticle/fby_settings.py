4b12471e6db5dc88e8c17e22611026f537e1ec84
Jonas G. Drange <jonas@drange.net>
2017-04-17 00:12:38 +0200

    settings now uses either local, immutable default config, or from disk

--- rpiparticle/fby_settings.py
+++ rpiparticle/fby_settings.py
@@ -1,13 +1,35 @@
 import json
+import os
 
 SETTINGS_PATH = '/usr/local/etc/friskby/friskby-settings.json'
 
 
+def get_settings_json():
+    settings = None
+    if (os.path.exists(SETTINGS_PATH)):
+        settings = open(SETTINGS_PATH)
+    else:
+        here = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__))
+        )
+        settings = open(os.path.join(here, 'friskby-settings.json'))
+
+    if not settings:
+        raise RuntimeError('Could not find any friskby-settings.json.')
+
+    return json.load(settings)
+
+
 def get_settings():
-    with open(SETTINGS_PATH) as settings:
-        return json.load(settings)
+    return get_settings_json()
 
 
 def get_setting(setting):
-    with open(SETTINGS_PATH) as settings:
-        return json.load(settings).get(setting, '')
+    return get_settings_json().get(setting, '')
+
+
+def set_setting(key, value):
+    settings_json = get_settings_json()
+    settings_json[key] = value
+    with open(SETTINGS_PATH, 'w+') as settings_file:
+        json.dump(settings_json, settings_file)
