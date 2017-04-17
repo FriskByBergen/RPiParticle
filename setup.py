c7ac6c779048f55f506d5b849e30d16a332dc252
Jonas G. Drange <jonas@drange.net>
2017-04-17 19:36:54 +0200

    bumps version to 0.9.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='RPiParticle',
-    version='0.9.1',
+    version='0.9.2',
     description='Raspberry Pi and SDS011 particle sensor posting data ',
     long_description=long_description,
     url='https://github.com/FriskByBergen/',
